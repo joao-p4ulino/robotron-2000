a7b08ce7c155eb6ab82a1ba283e704cced884302
João Paulino <joao.santos@ixcsoft.com.br>
2023-05-25 16:23:07 -0300

    feat: inserido id de controle das estatisticas

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
-const subtrair = document.querySelector('#subtrair');
-const somar = document.querySelector('#somar');
-const braco = document.querySelector('#braco');
 
+const controle = document.querySelectorAll('[data-controle]');
 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 20,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
-
-somar.addEventListener("click", (evento) => {
-
-    braco.value = parseInt(braco.value) + 1;
-
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        console.log(evento.target.parentNode)
+    })
 })
 
-subtrair.addEventListener("click", (evento) => {
+function manipulaDados (operacao, controle) {
+    const peca = controle.querySelector('[data-contador]');
 
-    braco.value = parseInt(braco.value) - 1;
-})
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    }else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
